70e31c710f075932658cc5aca8f9dc44748fabf8
WINDOWS-N6ALF94\sneha <snehapoddarnew@gmail.com>
2017-02-18 23:11:02 -0500

    changes

---
+++ js/agnes.js
@@ -0,0 +1,39 @@
+$(window).on('load',function () {
+    "use strict";
+    $("body").css("background-color", "black");
+    //alert("hello")
+  //   loadJSON(function(response) {
+	 //  // Parse JSON string into object
+	 //    var actual_JSON = JSON.parse(response);
+	 //    console.log("response is " + actual_JSON);
+ 	// });
+    //var json = JSON.parse();
+    //alert(json);
+ }); 
+
+
+
+function validate() {
+	var email = $('.emailInput').val();
+	var pass = $('.passwordInput').val();
+	//location.href = 'events.html';	
+	console.log(email);
+	console.log(pass);
+  	if((email === "kazem@groupten.com") && (pass ==="123456789"))
+    	location.href = 'events.html';		
+  	else
+    	return false;
+}
+// function loadJSON(callback) {   
+//     var xobj = new XMLHttpRequest();
+//     xobj.overrideMimeType("application/json");
+//     xobj.open('GET', '../AgnesExercise/events.json', true); // Replace 'my_data' with the path to your file
+//     xobj.onreadystatechange = function () {
+//           if (xobj.readyState == 4 && xobj.status == "200") {
+//             // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+//             callback(xobj.responseText);
+//           }
+//     };
+//     xobj.send(null);  
+//  }
+
--- js/plugins.js
+++ js/plugins.js
@@ -1,100 +1,23 @@
-$(document).ready(function(){
-    $('.navbar-brand img').css({
-               'opacity': '0'
-                
-            }); 
-});
 
+$(window).on('load',function () {
+     "use strict";
+     $("body").css("background-color", "black");
 
+ });       
+    
 /* Show and hide menu */
 
-$(document).ready(function(){
-   
-    'use strict';
-    
-    $(window).scroll(function() {
-        
-        'use strict';
-        
-        
-        if($(window).scrollTop() < 80 ) {
-            
-            $('.navbar').css ({
-               'margin-top': '-100px',
-                'opacity': '0'
-                
-            });
-            
-            $('.navbar-default').css({
-                'background-color': 'rgba(59, 59 , 59, 0)'
-                
-            });
-            
-           
-           
-            
-        } else {
-            
-            $('.navbar').css ({
-               'margin-top': '0px',
-                'opacity': '1'
-                
-            });
-            
-            $('.navbar-default').css({
-                'background-color': 'rgba(59, 59 , 59, 0.7)',
-                'border-color': '#444'
-                
-            });
-            
-            $('.navbar-brand img').css({
-               'height': '35px',
-                'padding-top': '0px',
-                'opacity': '1'
-                
-            });
-            
-            $('.navbar-nav > li > a ').css({
-                'padding-top': '15px'
-                
-                
-            });
-            
-            
-        }
-       
-        
-    });
-    
-    
-});
+
 
 // Loading Screen
 
-$(window).on('load',function () {
-    
-    "use strict";
-    
-    // Loading Elements
-    
-    $(".loading-overlay .sk-cube-grid").fadeOut(2000, function () {
-        
-        // Show The Scroll
 
-        $("body").css("overflow", "auto");
-        
-        $(this).parent().fadeOut(2000, function () {
-            
-            $(this).remove();
-        });
-    });
-});
 
 /*Counters*/
 $(document).ready(function(){
         $('.counter').counterUp({
-         delay: 10,
-         time: 1000
+         delay: 0,
+         time: 0
    });
 });
 
